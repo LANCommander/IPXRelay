49b98dee8f5964299aa200ee01c145f13e9f665b
Pat Hartl <pat@pathar.tl>
2024-01-06 20:54:26 -0600

    Change sample application to use console logger

--- IPXRelay.Server/Program.cs
+++ IPXRelay.Server/Program.cs
@@ -1,16 +1,27 @@
-﻿using System.Net.Sockets;
-using System.Net;
-using IPXRelayDotNet;
+﻿using IPXRelayDotNet;
 using Microsoft.Extensions.Logging;
 
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddFilter("IPXRelayDotNet", LogLevel.Trace)
+        .AddConsole();
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+
 // Starts a new IPX relay on port 213 by default
-using (var relay = new IPXRelay())
+using (var relay = new IPXRelay(logger))
 {
     Console.WriteLine($"Listening for UDP packets on port {relay.Port}...");
 
     relay.OnClientConnected += (sender, e) =>
     {
         Console.WriteLine($"Client connected from {e.RemoteEndPoint}!");
+    };
+
+    relay.OnReceivePacket += (sender, e) =>
+    {
+        Console.WriteLine($"Received packet from {e.RemoteEndPoint} | Network: {e.Packet.Header.DestinationAddress.Network}, Node: {e.Packet.Header.DestinationAddress.Node}, Socket: {e.Packet.Header.DestinationAddress.Socket}");
     };
 
     relay.OnReceivePacketError += (sender, e) =>
